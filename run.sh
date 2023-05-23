f62a1689f36cdd8194cb91daf2064aac32587ff5
MahirModan <107642456+MahirModan@users.noreply.github.com>
2023-05-23 15:58:49 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,5 @@
-#!/bin/sh
-touch aria2.log lazyleech.log
-tail -f aria2.log &
-tail -f lazyleech.log &
-# https://unix.stackexchange.com/a/230676
-export ARIA2_SECRET=$(tr -dc 'A-Za-z0-9!"#$%&'\''()*+,-./:;<=>?@[\]^_`{|}~' </dev/urandom | head -c 100)
-aria2c --enable-rpc=true "--rpc-secret=$ARIA2_SECRET" -j5 -x5 > aria2.log 2>&1 &
-python3 -m lazyleech > lazyleech.log 2>&1
+python3 -m venv venv
+
+. ./venv/bin/activate
+
+python3 -m lazyleech
