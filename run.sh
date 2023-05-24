0550342365074186fc617f0189f4e39b0d09bee8
MahirModan <107642456+MahirModan@users.noreply.github.com>
2023-05-24 08:40:04 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,19 @@
-python3 -m venv venv
+WORKDIR /usr/src/app
 
-. ./venv/bin/activate
+RUN chmod 777 /usr/src/app
 
-python3 -m lazyleech
+COPY requirements.txt .
+
+RUN pip3 install --no-cache-dir -r requirements.txt
+
+RUN apt-get update && apt-get upgrade -y
+
+RUN apt -qq update --fix-missing && \
+
+    apt -qq install -y mediainfo
+
+COPY . .
+
+CMD ["bash", "start.sh"]
+
+
